2557da4194e17f4caf095060373fc43af234e4ab
Eda Dursun <edadrsn929@gmail.com>
2025-04-21 22:54:14 +0300

    new buttons add,functions defined,xml views changed

--- app/src/main/java/com/example/catchkennykotlin/MainActivity.kt
+++ app/src/main/java/com/example/catchkennykotlin/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityMainBinding
     lateinit var sharedPreferences:SharedPreferences
+    lateinit var selectedKenny:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +24,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         sharedPreferences=getSharedPreferences("com.example.catchkennykotlin",Context.MODE_PRIVATE)
+        val orangeKenny=binding.orangeKenny.toString()
+        val pinkKenny=binding.pinkKenny.toString()
+        val blueKenny=binding.blueKenny.toString()
+        binding.btnStart.isEnabled=false
 
+    }
+
+    fun btnOrange(view:View){
+        selectedKenny="orange"
+        binding.btnStart.isEnabled=true
+    }
+
+    fun btnPink(view:View){
+        selectedKenny="pink"
+        binding.btnStart.isEnabled=true
+    }
+
+    fun btnBlue(view:View){
+        selectedKenny="blue"
+        binding.btnStart.isEnabled=true
     }
 
     fun btnStart(view: View){
         val intent= Intent(this@MainActivity,MainActivity2::class.java)
-        sharedPreferences.edit().putString("user_name",binding.nameText.text.toString()).apply()
-        sharedPreferences.edit().putString("user_email",binding.mailText.text.toString()).apply()
+        intent.putExtra("selectedKenny",selectedKenny)
         startActivity(intent)
 
     }
+
+    fun save(view:View){
+
+    }
+
+
+    fun delete(view:View){
+        
+    }
+
+    // sharedPreferences.edit().putString("user_name",binding.nameText.text.toString()).apply()
+    //        sharedPreferences.edit().putString("user_email",binding.mailText.text.toString()).apply()
 }
